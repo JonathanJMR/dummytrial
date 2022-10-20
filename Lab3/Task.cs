4d2f794b6c88ea20f3b4d90a00effcd399a38e12
Jonathan-Joseph Roy <2186385@johnabbottcollege.net>
2022-10-20 09:56:45 -0400

    added code needed for Lab3

---
+++ Lab3/Task.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    internal class Task
+    {
+        #region Backing Fields
+
+        private string _summary;
+        private string _details;
+        private DateTime _dueDate;
+        private bool _completed;
+
+        #endregion
+
+        #region Properties
+
+        public string Summary
+        {
+            get { return _summary; }
+            set
+            {
+                if(value == null || value == "")
+                {
+                    throw new ArgumentNullException("The value was not set.","value");
+                }
+                else
+                {
+                    this._summary = value;
+                }
+                    
+            }
+
+        }
+
+        public string Details
+        {
+            get { return _details; }
+            set
+            {
+                if (value == null)
+                    this._details = "";
+
+            }
+            
+        }
+
+        public DateTime DueDate
+        {
+            get { return _dueDate; }
+            set
+            {
+                if (value < DateTime.Now.AddDays(-30))
+                {
+                    throw new ArgumentException("The object cannot older than a month", "Due Date");
+                }
+                else
+                {
+                    this.DueDate = value;
+                }
+            }
+        }
+
+        public bool IsCompleted
+        {
+            get { return _completed; }
+            set
+            {
+                this._completed = value;
+            }
+
+        }
+
+        public bool IsOverdue
+        {
+            get { return (DateTime.Now > DueDate); } 
+        }
+
+        public string Info
+        {
+            get { return $"Task: {Summary} - Due Date: {DueDate} - {IsCompleted} - {IsOverdue}"; }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        // there is no need for a default constructor because the summary cannot be empty or null,
+        // therefore the summary must have an inputted value. with the following construction below.
+        public Task(string summary) // sets the summary data field and sets details to a default value.
+        {
+            _summary = summary;
+            _details = Details;
+        }
+
+        public Task(string summary, DateTime dueDate) // sets summary and details field with inputs
+        {
+            this._summary = summary;
+            this._dueDate = dueDate;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public override string ToString()
+        {
+            return Info;
+        }
+
+        #endregion
+
+    }
+}
---
+++ Lab3/TaskScheduler.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class TaskScheduler
+    {
+        private readonly List<Task> _tasks;
+
+        #region Properties
+
+        public List<Task> Tasks
+        {
+            get { return _tasks; }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return _tasks.Count;
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public TaskScheduler()
+        {
+            _tasks = new List<Task>(); 
+        }
+
+        public TaskScheduler(List<Task>task)
+        {
+            _tasks = task;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public List<Task> AddTask(Task newTask)
+        {
+            if(newTask == null)
+            {
+                return Tasks;
+            }
+            else
+            {
+                Tasks.Add(newTask);
+                return Tasks;
+            }
+        }
+        
+
+        public void ClearScheduler()
+        {
+            this._tasks.Clear();
+        }
+
+        #endregion
+    }
+}
